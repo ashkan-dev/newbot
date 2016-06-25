cd3707439034de79268fecdc5da7e6e6b4ec23fe
allwenandashi <ashkanpj.ap55@gmail.com>
2016-06-25 17:22:09 +0430

    Update timefa.lua

--- plugins/timefa.lua
+++ plugins/timefa.lua
@@ -1,7 +1,7 @@
 local address = 'http://powerful.friends-memorial.ir/api/'
 local function run(msg)
 local resolve = http.request(address..'index.php')
-resolve = string.gsub(resolve,'#Allwen','#ElementTG')
+resolve = string.gsub(resolve,'#Ashi','#worldtg')
 return resolve
 end
 return {
