28e01058e53efa4377d15f5da36a8ebd2c79b537
allwenandashi <ashkanpj.ap55@gmail.com>
2016-06-25 17:20:44 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (element)",
-	"^[#!/]([Ee]lement)"
+    "^[#!/]([Ss]tats) (world)",
+	"^[#!/]([Ww]orld)"
     }, 
   run = run
 }
