863b657c1310777d8312102b970229ca06073216
allwenandashi <ashkanpj.ap55@gmail.com>
2016-08-30 16:24:40 +0300

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -3,13 +3,13 @@ do
 function run(msg, matches)
 local reply_id = msg['id']
 
-local info = '~This Is Your & Gp Info !\n~Your Name: -> '..msg.from.first_name..'\n'\n'
-..'~Your Id : -> '..msg.from.id..'\n'\n'
-..'~Your Username : -> @'..msg.from.username..'\n'\n'
-..'~Group Id : -> '..msg.to.id..'\n'\n'
-..'~Group name : -> '..msg.to.title..'\n'\n'
-..'~Your Special Link : -> https://telegram.me/'..msg.from.username..'\n'\n'
-..'~Your Phone : +'..msg.from.phone..'\n'
+local info = '~This Is Your & Gp Info !\n~Your Name: -> '..msg.from.first_name..'\n'
+..'~ایدی عددی شما : -> '..msg.from.id..'\n'
+..'~ایدی شما : -> @'..msg.from.username..'\n'
+..'~ایدی عددی گروه : -> '..msg.to.id..'\n'
+..'~اسم شما : -> '..msg.to.title..'\n'
+..'~لینک تلگرام شما : -> https://telegram.me/'..msg.from.username..'\n'
+..'~شماره تلفن شما : +'..msg.from.phone..'\n'
 
 reply_msg(reply_id, info, ok_cb, false)
 end
