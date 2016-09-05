93c4547531268c922d5481e4a3576901cdc7dc38
allwenandashi <ashkanpj.ap55@gmail.com>
2016-09-05 17:35:24 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -205,7 +205,7 @@ else
 
    rm -r ../.telegram-cli/state
 
-   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/element.lua -l 1 -E $@
+   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/seedbot.lua -l 1 -E $@
 
    sleep 3
 
