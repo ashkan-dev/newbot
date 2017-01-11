0e65e6e6bd407421fedf39aa0b198ce1115bbe85
ashkan-dev <ashkanpiri2@gmail.com>
2017-01-11 09:44:09 +0330

    Update dad.lua

--- plugins/dad.lua
+++ plugins/dad.lua
@@ -1,9 +1,9 @@
 do
 
 function run(msg, matches)
-local reply_id = msg['260871317']
+local reply_id = msg['265821275']
 local text = 'با باباییم چیکار داری؟❤'
-if matches[1] == 'ashi' then
+if matches[1] == 'ashkan' then
     if is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
 end
@@ -11,7 +11,7 @@ end
 end
 return {
 patterns = {
-    "^([Aa][Ss][Hh][Ii])$",
+    "^([Aa][Ss][Hh][Kk][Aa][Nn])$",
 },
 run = run
 }
